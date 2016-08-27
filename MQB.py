6555cabf5d4ef4d6a1275907e282621fb6731246
artemis <artemis.linhart@gmail.com>
2016-08-27 14:47:26 +0200

    added more moods

--- MQB.py
+++ MQB.py
@@ -38,7 +38,38 @@ def silence(bot, update):
 def idontcare(bot, update):
     bot.sendPhoto(update.message.chat_id, photo='http://67.media.tumblr.com/4174671f266454da747190b386a265f6/tumblr_ockh8oJJaC1vewk1qo1_1280.jpg')
 
+def sad(bot, update):
+    bot.sendPhoto(update.message.chat_id, photo='http://data.whicdn.com/images/85554305/large.png')
 
+def comfort(bot, update):
+    bot.sendPhoto(update.message.chat_id, photo='http://65.media.tumblr.com/tumblr_lya3m9SGHB1qzdstpo1_500.jpg')
+
+def lonely(bot, update):
+    bot.sendPhoto(update.message.chat_id, photo='http://65.media.tumblr.com/tumblr_lya4mtvSd41qzdstpo1_500.jpg')
+
+def alcohol(bot, update):
+    bot.sendPhoto(update.message.chat_id, photo='http://67.media.tumblr.com/tumblr_lb5pm0pepw1qao8ddo1_500.jpg')
+
+def lazy(bot, update):
+    bot.sendPhoto(update.message.chat_id, photo='http://67.media.tumblr.com/tumblr_lbzk5pbQso1qzdstpo1_500.jpg')
+
+def desperate(bot, update):
+    bot.sendPhoto(update.message.chat_id, photo='http://67.media.tumblr.com/tumblr_l396v15lkN1qax7yao1_500.png')
+
+def unmotivated(bot, update):
+    bot.sendPhoto(update.message.chat_id, photo='http://67.media.tumblr.com/tumblr_l396v15lkN1qax7yao1_500.png')
+
+def agree(bot, update):
+    bot.sendPhoto(update.message.chat_id, photo='http://67.media.tumblr.com/tumblr_l3pbo60QXN1qzdstpo1_500.jpg')
+
+def angry(bot, update):
+    bot.sendPhoto(update.message.chat_id, photo='http://67.media.tumblr.com/tumblr_kyd6x8zizu1qzdstpo1_500.png')
+
+def anger(bot, update):
+    bot.sendPhoto(update.message.chat_id, photo='http://67.media.tumblr.com/tumblr_kyd6x8zizu1qzdstpo1_500.png')
+
+def frustration(bot, update):
+    bot.sendPhoto(update.message.chat_id, photo='http://67.media.tumblr.com/tumblr_kxp4djSiDE1qa0ideo1_500.jpg')
 
 
 # Helpers
@@ -61,5 +92,16 @@ dispatcher.add_handler(caps_handler)
 dispatcher.add_handler(CommandHandler('icebreaker', icebreaker))
 dispatcher.add_handler(CommandHandler('idontcare', idontcare))
 dispatcher.add_handler(CommandHandler('silence', silence))
+dispatcher.add_handler(CommandHandler('sad', sad))
+dispatcher.add_handler(CommandHandler('lonely', lonely))
+dispatcher.add_handler(CommandHandler('lazy', lazy))
+dispatcher.add_handler(CommandHandler('alcohol', alcohol))
+dispatcher.add_handler(CommandHandler('desperate', desperate))
+dispatcher.add_handler(CommandHandler('unmotivated', unmotivated))
+dispatcher.add_handler(CommandHandler('comfort', comfort))
+dispatcher.add_handler(CommandHandler('agree', agree))
+dispatcher.add_handler(CommandHandler('angry', angry))
+dispatcher.add_handler(CommandHandler('anger', anger))
+dispatcher.add_handler(CommandHandler('frustration', frustration)
 
 updater.start_polling()
