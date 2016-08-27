267e400b454f79c8c03cfbb3dbb99af6d8775cfc
Kira <loskiros.music@gmail.com>
2016-08-27 14:12:49 +0200

    add two more images

--- MQB.py
+++ MQB.py
@@ -32,6 +32,13 @@ def caps(bot, update, args):
 def icebreaker(bot, update):
     bot.sendPhoto(update.message.chat_id, photo='http://67.media.tumblr.com/19ac78e1747fe210c164aa12882bbc35/tumblr_nd23pns3Z41rx3q30o1_500.gif')
 
+def silence(bot, update):
+    bot.sendPhoto(update.message.chat_id, photo='http://67.media.tumblr.com/05ab4de70d2195e3ba0b2338eca31005/tumblr_o0on44BT1l1uhipc8o1_1280.jpg')
+
+def idontcare(bot, update):
+    bot.sendPhoto(update.message.chat_id, photo='http://67.media.tumblr.com/4174671f266454da747190b386a265f6/tumblr_ockh8oJJaC1vewk1qo1_1280.jpg')
+
+
 
 
 # Helpers
@@ -40,6 +47,7 @@ echo_handler = MessageHandler([Filters.text], echo)
 caps_handler = CommandHandler('caps', caps, pass_args=True)
 
 updater = Updater('221355228:AAEaNilNWb1_P32QSEXhovNVJTzO8aZX-4g')
+
 
 # For quicker access to the Dispatcher used by your Updater
 dispatcher = updater.dispatcher
@@ -51,5 +59,7 @@ dispatcher.add_handler(CommandHandler('help', help))
 dispatcher.add_handler(echo_handler)
 dispatcher.add_handler(caps_handler)
 dispatcher.add_handler(CommandHandler('icebreaker', icebreaker))
+dispatcher.add_handler(CommandHandler('idontcare', idontcare))
+dispatcher.add_handler(CommandHandler('silence', silence))
 
 updater.start_polling()
